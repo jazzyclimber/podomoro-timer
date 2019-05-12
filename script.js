f42053317ab0621e41f53bbd68aeca3819fb78b5
Niccolas Maganeli <niccomaganeli@hotmail.com>
2019-05-12 19:56:09 -0300

    Displays change functionality & encapsulated repeatable code

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
+// ------------ Global Constants ----------------
+const WORK_PERIOD = 0,
+      REST_PERIOD = 1;
+
 // ------------ Global Variables ----------------
 let startButton = document.querySelector("#play"),
   pauseButton = document.querySelector("#pause"),
   resetButton = document.querySelector("#repeat"),
-  work_display = document.querySelector("#work-time"),
-  rest_display = document.querySelector("#rest-time"),
-  timer = 60 * 25,
-  rest_timer = 5 * 1,
+  displays = [document.querySelector("#work-time"), document.querySelector("#rest-time")],
+  work_time = 60 * 25,
+  rest_time = 60 * 5,
+  times = [work_time, rest_time],
   interval = null,
   min, sec;
 
 // Guard Variables
-let workPeriod = work_display.classList.contains("active") ? true : false;
+let actual_period = displays[WORK_PERIOD].classList.contains("active") ? WORK_PERIOD : REST_PERIOD;
 
 // ------------ Main Functions -------------------
 
+// Checks if actual period has ended
+function isOver(actual_time) {
+  return actual_time === 0;
+}
+
+// Change displays
+function changeDisplays() {
+  for(let i = 0; i < displays.length; i++) {
+    if(displays[i].className === 'active') {
+      displays[i].className = 'passive';
+    } else {
+      displays[i].className = 'active';
+    }
+  }
+  actual_period = (actual_period + 1) % displays.length;
+}
+
 // Counts Down Whatever Time is in Timer
 function countDown() {
-  if (!interval && workPeriod == true ) {
-    startWork = setInterval(function () {
-      setFormatTime(--timer, work_display);
+  if(!interval) {
+    interval = setInterval(function() {
+      setFormatTime(--times[actual_period], displays[actual_period]);
+      if (isOver(times[actual_period])) {
+        setPause();
+        resetTimer();
+        changeDisplays();
+        countDown();
+      }
     }, 1000);
-
-    interval = startWork;
-
-  } else if (!interval && workPeriod == false) {
-    startRest = setInterval(function () {
-      setFormatTime(--rest_timer, rest_display);
-    }, 1000);
-
-    interval = startRest;
   }
 }
 
@@ -49,17 +67,16 @@ function setPause() {
 
 // Resets interval
 function resetTimer() {
-  timer = 60 * 25;
-  rest_timer = 60 * 5;
-
+  times[WORK_PERIOD] = work_time;
+  times[REST_PERIOD] = rest_time;
   interval ? setPause() : null ;
-  setFormatTime(timer, work_display);
-  setFormatTime(rest_timer, rest_display)
+  for(let i = 0; i < displays.length; i++) {
+    setFormatTime(times[i], displays[i]);
+  }
 }
 
 //---------- Functions to call ------------------
-setFormatTime(timer, work_display);
-setFormatTime(rest_timer, rest_display);
+resetTimer();
 startButton.addEventListener('click', countDown);
 pauseButton.addEventListener('click', setPause);
 resetButton.addEventListener('click', resetTimer);
